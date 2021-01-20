b0a7ca9c79637018395635a2b54d698027831236
PandelisT <pandeli@nerdypandy.com>
2021-01-20 14:33:22 +1100

    Get list of journal entries working

--- src/controllers/journal.py
+++ src/controllers/journal.py
@@ -13,6 +13,7 @@ journal = Blueprint("journal", __name__, url_prefix="/journal")
 @login_required
 def journal_entry_create():
     form = AddJournalEntryForm()
+    print(form.validate_on_submit())
     if form.validate_on_submit():
         journal = Journal()
         journal.journal_entry = form.journal_entry.data
@@ -26,6 +27,7 @@ def journal_entry_create():
 @journal.route("/journal_entries", methods=["GET"])
 @login_required
 def get_journal_entries():
-
-    journal_entries = Journal.query.all()
+    user_id = current_user._get_current_object()
+    user = user_id.id
+    journal_entries = Journal.query.filter_by(user_id_fk=user).all()
     return render_template('journal_entries.html', journal_entries=journal_entries)
