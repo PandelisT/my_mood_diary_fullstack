f6e70d4d3fc12edec0d32db40d393abc12fe3826
PandelisT <pandeli@nerdypandy.com>
2021-02-22 13:10:59 +1100

    Added more automated tests for deleting and added flake8 and coverage to requirements.txt

--- src/tests/test_journal.py
+++ src/tests/test_journal.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 from main import create_app, db
+from models.Journal import Journal
 
 
 class TestAuthMoodApp(unittest.TestCase):
@@ -12,9 +13,7 @@ class TestAuthMoodApp(unittest.TestCase):
         cls.app_context = cls.app.app_context()
         cls.app_context.push()
         cls.client = cls.app.test_client()
-
         db.create_all()
-
         runner = cls.app.test_cli_runner()
         runner.invoke(args=["db", "seed"])
 
@@ -62,3 +61,27 @@ class TestAuthMoodApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("Journal entries", str(response.data))
         self.assertIn("test entry", str(response.data))
+
+    def test_delete_journal(self):
+        response = self.client.post('/signup', data={
+            'email': 'pandeli@test.com',
+            'name': 'Pandelis',
+            'password': 'testing'
+        })
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post('/login', data={
+            'email': 'pandeli@test.com',
+            'password': 'testing'
+        }, follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post("/journal/new_entry", data={
+            'journal_entry': 'test entry',
+        }, follow_redirects=True)
+
+        journal_entry = Journal.query.first()
+        response = self.client.post(f"journal/journal-entries/{journal_entry.id}", follow_redirects=True)
+               
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn("test entry", str(response.data))
